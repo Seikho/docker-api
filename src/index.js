b420310a6ca42403f773408b2ee393ef91d420bb
Carl Winkler <carl@carlossus.com>
2015-06-04 16:40:26 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,53 @@
+var Promise = require("bluebird");
+var path = require("path");
+var https = require("https");
+var fs = require("fs");
+var readFileAsync = Promise.promisify(fs.readFile);
+var cwd = path.resolve(__dirname);
+// TODO: Docker API URL should be in config
+// TODO: Docker API version should be in config
+// TODO: Ensure API requests are prefixed with a forward-slash
+// TODO: Ensure docker() works as intended on *nix systems
+function boot2docker(request, method) {
+    var options = dockerOptions(request, method, "192.168.59.103");
+    // Windows Docker API requires SSL information
+    var keyPromise = readFileAsync(path.join(cwd, "key.pem"))
+        .then(function (key) { options.key = key.toString(); });
+    var certPromise = readFileAsync(path.join(cwd, "cert.pem"))
+        .then(function (cert) { options.cert = cert.toString(); });
+    var caPromise = readFileAsync(path.join(cwd, "ca.pem"))
+        .then(function (ca) { options.ca = ca.toString(); });
+    return Promise.all([keyPromise, certPromise, caPromise]).then(function () {
+        return dockerPromise(options, https.request);
+    });
+}
+exports.boot2docker = boot2docker;
+function dockerPromise(options, requestApi) {
+    var dockerResponse = "";
+    return new Promise(function (resolve, reject) {
+        var req = requestApi(options, function (response) {
+            response.on("data", function (data) { return dockerResponse += data.toString(); });
+            response.on("end", function (data) {
+                var dockerJson = JSON.parse(dockerResponse);
+                resolve(Promise.resolve(dockerJson));
+            });
+        });
+        req.on("error", function (err) { return reject(err); });
+        req.end();
+    });
+}
+function dockerOptions(request, method, url, port) {
+    var options = {
+        key: "",
+        cert: "",
+        ca: "",
+        hostname: url || "192.168.59.103",
+        port: port || 2376,
+        method: method || "GET",
+        path: request
+    };
+    if (options.method !== "GET" && options.method !== "POST")
+        options.method = "GET";
+    return options;
+}
+boot2docker("/images/json").then(console.log);
---
+++ src/index.ts
@@ -0,0 +1,66 @@
+import Promise = require("bluebird");
+import path = require("path");
+import https = require("https");
+import http = require("http");
+import fs = require("fs");
+
+var readFileAsync = Promise.promisify(fs.readFile);
+var cwd = path.resolve(__dirname);
+
+// TODO: Docker API URL should be in config
+// TODO: Docker API version should be in config
+// TODO: Ensure API requests are prefixed with a forward-slash
+// TODO: Ensure docker() works as intended on *nix systems
+// TODO: key/cert/ca location should be configurable
+
+export function boot2docker(request: string, method?: string) {
+	var options = dockerOptions(request, method, "192.168.59.103");
+	
+	// Windows Docker API requires SSL information
+	var keyPromise = readFileAsync(path.join(cwd, "key.pem"))
+		.then(key => { options.key = key.toString(); });
+
+	var certPromise = readFileAsync(path.join(cwd, "cert.pem"))
+		.then(cert => { options.cert = cert.toString(); });
+
+	var caPromise = readFileAsync(path.join(cwd, "ca.pem"))
+		.then(ca => { options.ca = ca.toString(); });
+	
+	return Promise.all([keyPromise, certPromise, caPromise]).then(() => {
+		return dockerPromise(options, https.request);
+	});
+}
+
+function dockerPromise(options: https.RequestOptions, requestApi: any) {
+	var dockerResponse = "";
+
+	return new Promise((resolve, reject) => {
+		var req = requestApi(options, response => {
+			response.on("data", data => dockerResponse += data.toString());
+			response.on("end", data => {
+				var dockerJson = JSON.parse(dockerResponse);
+				resolve(Promise.resolve(dockerJson))
+			});
+		});
+		req.on("error", err => reject(err));
+		req.end();
+	});
+}
+
+function dockerOptions(request: string, method?: string, url?: string, port?: number): https.RequestOptions {
+	var options = {
+		key: "",
+		cert: "",
+		ca: "",
+		hostname: url || "192.168.59.103",
+		port: port || 2376,
+		method: method || "GET",
+		path: request
+	};
+
+	if (options.method !== "GET" && options.method !== "POST") options.method = "GET";
+
+	return options;
+}
+
+boot2docker("/images/json").then(console.log);
